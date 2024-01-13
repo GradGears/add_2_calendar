d29a70df20efeb40e31ee8496b837a7348179e88
Dan Kurtz <andfurthermore@gmail.com>
2024-01-12 16:34:38 -0800

    Ensure calendar completion completes

--- ios/Classes/SwiftAdd2CalendarPlugin.swift
+++ ios/Classes/SwiftAdd2CalendarPlugin.swift
@@ -104,6 +104,7 @@ public class SwiftAdd2CalendarPlugin: NSObject, FlutterPlugin {
             OperationQueue.main.addOperation {
                 self.presentEventCalendarDetailModal(event: event, eventStore: eventStore)
             }
+            completion?(true)
         } else {
             let authStatus = getAuthorizationStatus()
             switch authStatus {
